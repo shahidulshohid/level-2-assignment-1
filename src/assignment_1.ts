8219b7e81d2e01fa337df3cd5a03836829346e4e
shahidul shohid <shahidulislamshohid7@gmail.com>
2025-05-08 02:58:25 +0600

    5 number problem is solve

--- src/assignment_1.ts
+++ src/assignment_1.ts
@@ -64,6 +64,17 @@ const myCar = new Car("Toyota", 2020, "Corolla" )
 // console.log(myCar.getModel())
 
 //Problem 5
+function processValue(value: string | number): number {
+    if(typeof value === "string") {
+        return value.length;
+    }
+    else {
+        return value * 2;
+    }
+}
+
+console.log(processValue("hello")); 
+console.log(processValue(10));     
 
 
 
