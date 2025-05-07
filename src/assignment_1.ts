61615a8dae85f033c8564739a07f05aad50f2074
shahidul shohid <shahidulislamshohid7@gmail.com>
2025-05-08 00:47:40 +0600

    2 number problem is solve

--- src/assignment_1.ts
+++ src/assignment_1.ts
@@ -8,14 +8,26 @@ function formatString(input: string, toUpper?: boolean): string {
   }
 }
 
-console.log(formatString("Hello"));
-console.log(formatString("Hello", true));
-console.log(formatString("Hello", false));
+// console.log(formatString("Hello"));
+// console.log(formatString("Hello", true));
+// console.log(formatString("Hello", false));
 
 //problem 2
 
+function filterByRating (items: { title: string; rating: number }[]) : { title: string; rating: number }[] {
+let result = items.filter((item => item.rating >= 4 ))
+return result
+}
+
 const books = [
-  { title: "Book A", rating: 4.5 },
-  { title: "Book B", rating: 3.2 },
-  { title: "Book C", rating: 5.0 },
-];
+    { title: "Book A", rating: 4.5 },
+    { title: "Book B", rating: 3.2 },
+    { title: "Book C", rating: 5.0 },
+  ];
+
+console.log(filterByRating(books))
+
+
+
+
+
